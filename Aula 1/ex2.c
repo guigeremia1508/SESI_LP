c4a1662f927f98b07797cef4c4d8d84ac00c46ac
Guilherme Augusto <guilherme.geremia@aluno.senai.br>
2026-03-30 10:33:30 -0300

    aula1

---
+++ Aula 1/ex2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    float n1,n2,soma;
+    
+    printf("digite um numero: ");
+    scanf("%f", &n1);
+    
+    printf("digite o segundo numero: ");
+    scanf("%f", &n2);
+    
+    soma = n1 + n2;
+    
+    printf("%.2f + %.2f = %.2f", n1, n2, soma);
+}
